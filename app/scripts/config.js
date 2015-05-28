7880c2abd17c3b04268ba4c11a6bb8b5e4486f1d
Afshin Teymoori <afshin_tm@yahoo.com>
2015-05-28 16:46:17 +1000

    Create develop branch

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 "use strict";
 
  angular.module('config', [])
-//.constant('ENV', {name:'development',apiEndpoint:'http://localhost/BackendServices/api'})
-.constant('ENV', {name:'development',apiEndpoint:'http://windows/ProductsApi/api'});
+.constant('ENV', {name:'development',apiEndpoint:'http://localhost/BackendServices/api'})
+//.constant('ENV', {name:'development',apiEndpoint:'http://windows/ProductsApi/api'});
