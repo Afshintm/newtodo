3342d86b23622accab32b37b4243cb75ec257fa8
Afshin Teymoori <afshin_tm@yahoo.com>
2015-10-12 14:12:27 +1100

    Add Employee provider which wrap a javascript class inside an angular provider

--- app/scripts/controllers/invoiceCtrl.js
+++ app/scripts/controllers/invoiceCtrl.js
@@ -1,7 +1,9 @@
 'use strict';
-angular.module('mytodoApp').controller('invoiceCtrl',['$scope',function($scope){
+angular.module('mytodoApp').controller('invoiceCtrl',['$scope','Employee',function($scope, Employee){
 	var model = $scope.model = {
 		viewTitle:'My Invoices'
 	};
+	model.employee = new Employee('afshin', 42);
+	
 	// console.log(model.viewTitle);
 }]);
---
+++ app/scripts/services/Employee.js
@@ -0,0 +1,126 @@
+// 'use strict';
+// function Charge(montlyCharge, setupCharge){
+
+// 		this.montlyCharge = montlyCharge ;
+// 		this.setupCharge = setupCharge ;
+// 		this.isMonthlyEditable = true;
+// 		this.IsSetupEditable = true ;
+// };
+	
+// angular.module('dealerPortalApp').factory('Charge',[function (montlyCharge,setupCharge){
+// 	'use strict';
+// 	return new Charge(montlyCharge,setupCharge) ;
+// }]);
+
+
+//=========================================================
+// 'use strict';
+// function Charge(montlyCharge, setupCharge){
+
+// 		this.montlyCharge = montlyCharge ;
+// 		this.setupCharge = setupCharge ;
+// 		this.isMonthlyEditable = true;
+// 		this.IsSetupEditable = true ;
+// };
+// angular.module('dealerPortalApp').factory('Charge',[function (){
+// 	'use strict';
+// 	return function(montlyCharge,setupCharge){
+// 		 return new Charge(montlyCharge,setupCharge) ;
+// 	};
+// }]);
+
+//Wrapping Employee class within angular provider
+
+//=========================================================
+angular.module('mytodoApp').provider('Employee',[function EmployeeProvider(){
+	'use strict';
+	
+	function Employee(name,age){
+			this.name = name;
+			this.age = age;
+			this.isEnabled = true;
+	}
+
+	Employee.prototype.getFullDetails = function(){
+		return (this.name +' ' + this.age+ ' years old.');
+	};
+
+	Employee.prototype.setAge = function(value){
+			this.age = value;
+	};
+
+
+	Employee.build = function(data){
+		return new Employee(data.name, data.age);
+	};
+
+	this.$get = [function(){
+		return Employee;
+	}];
+}]);
+
+
+
+//=========================================================
+//Wrapping Charge class within angular factory
+//=========================================================
+
+// angular.module('dealerPortalApp').factory('Charge',[function () {
+ 
+//   /**
+//    * Constructor, with class name
+//    */
+//   function Charge(montlyCharge, setupCharge, isMonthlyEditable, isSetupEditable) {
+//     // Public properties, assigned to the instance ('this')
+//     this.montlyCharge = montlyCharge || 0;
+//     this.setupCharge = setupCharge || 0;
+//     this.isMonthlyEditable = isMonthlyEditable || true;
+//     this.isSetupEditable = isSetupEditable || true;
+//   }
+ 
+//   /**
+//    * Public method, assigned to prototype
+//    */
+//   Charge.prototype.getFullCharge = function () {
+//     return this.montlyCharge + this.setupCharge;
+//   };
+ 
+//   /**
+//    * Private property
+//    */
+//   var possibleRoles = ['admin', 'editor', 'guest'];
+ 
+//   /**
+//    * Private function
+//    */
+//   function checkRole(role) {
+//     return possibleRoles.indexOf(role) !== -1;
+//   }
+ 
+//   /**
+//    * Static property
+//    * Using copy to prevent modifications to private property
+//    */
+//   Charge.possibleRoles = angular.copy(possibleRoles);
+ 
+//   /**
+//    * Static method, assigned to class
+//    * Instance ('this') is not available in static context
+//    */
+//   Charge.build = function (data) {
+//     // if (!checkRole(data.role)) {
+//     //   return;
+//     // }
+//     return new Charge(
+//       data.montlyCharge,
+//       data.setupCharge,
+//       data.isMonthlyEditable,
+//       data.isSetupEditable
+//     );
+//   };
+ 
+//   /**
+//    * Return the constructor function
+//    */
+//   return Charge;
+// }]);
