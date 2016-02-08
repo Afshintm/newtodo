c25a5ff4a72a23da32aa1511d75a18de491d7a55
Afshin Teymoori <afshin_tm@yahoo.com>
2016-02-09 09:47:55 +1100

    change custometr factory

--- app/scripts/factories/customers.js
+++ app/scripts/factories/customers.js
@@ -3,47 +3,22 @@ angular.module('mytodoApp')
 .factory('Customers',['utils','firebaseRef','fbjomonCustomersUrl','$q',function CustomersFactory(utils,firebaseRef,fbjomonCustomersUrl,$q){
     var ref = firebaseRef(fbjomonCustomersUrl);
 
-    console.log(ref);
-
     var customerArray = utils.getFirebaseArray(ref) ;
 
-    var getdata = function(){
-            var defered = $q.defer() ;
-            customerArray
-            .then(function(firebaseData){
-                defered.resolve(firebaseData);
-            })
-            .catch(function(error){
-                console.error(error);
-                defered.reject(error);
-
-            });
-            return defered.promise;
-        };
     var customer = {
         getAll: function(){
-            var defered = $q.defer() ;
-            customerArray
-            .then(function(firebaseData){
-                defered.resolve(firebaseData);
-            })
-            .catch(function(error){
-                console.error(error);
-                defered.reject(error);
-
-            });
-            return defered.promise;
+            return customerArray ;
         },
         getById: function(id){
             var defered = $q.defer();
-            getdata().then(function(data){
+            customerArray.then(function(data){
                 defered.resolve(data.$getRecord(id));
             });
             return defered.promise ;
         },
         remove: function(id){
             this.getById(id).then(function(record){
-                getdata().then(function(data){
+                customerArray.then(function(data){
                     data.$remove(record);
                 });
             });
@@ -51,7 +26,7 @@ angular.module('mytodoApp')
         },
         insert: function(customer){
             var defered = $q.defer();
-            getdata().then(function(data){
+            customerArray.then(function(data){
                 data.$add(customer).then(function(ref){
                     defered.resolve(data.$indexFor(ref.key()));
                 });
@@ -60,8 +35,7 @@ angular.module('mytodoApp')
         },
         update: function(customer){
             var defered = $q.defer() ;
-            var item = this.getAll();
-            item.then(function(data){
+            customerArray.then(function(data){
                 data.$save(customer).then(function(ref) {
                     var refKey = ref.key();
                     console.log("Updated record with id " + refKey);
@@ -71,7 +45,6 @@ angular.module('mytodoApp')
                 });
             });
             return defered.promise;
-
         }
 
     };
